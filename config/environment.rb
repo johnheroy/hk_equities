cf3bce74b6b973b6727a18de1e4df5ca2e669916
John Heroy <johnheroy@gmail.com>
2014-07-07 10:45:02 +0800

    Add default encoding for utf8

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
