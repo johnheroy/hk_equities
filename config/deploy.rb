202e0a9cf688cca4c4ca873cc8f6b1034ae3cbef
John Heroy <johnheroy@gmail.com>
2014-10-08 22:47:25 -0700

    Add config/secrets.yml to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/home/rails'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
